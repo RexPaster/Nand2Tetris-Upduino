a4f6b4db0619b8a96839873c576bc13a9d489133
Rex Paster <p.rex@wustl.edu>
2026-03-27 03:54:29 +0000

    Final Script (Debug Version

--- build-oss-cad-suite.sh
+++ build-oss-cad-suite.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=ib-linuxlab
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=48G
 #SBATCH --time=04:00:00
 #SBATCH -A engr-class-any
 
